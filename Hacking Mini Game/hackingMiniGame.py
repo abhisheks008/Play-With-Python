d4d68121fb40a8afb193388b3b76390a35d4dc7e
SRIHARI S <sshsrn@gmail.com>
2022-12-21 09:40:22 +0530

    feat: added images to readme
    Signed-off-by: SRIHARI S <sshsrn@gmail.com>

--- Hacking Mini Game/hackingMiniGame.py
+++ Hacking Mini Game/hackingMiniGame.py
@@ -28,16 +28,16 @@ def getHint(word, guess):
 def main():
     while(True):
         print("""
-    Welcome to the Hacking mini game!
-    The objective of the game is to find the password in the computer's memory.
-    You must guess a 7 letter word.
-    You have 4 guesses.
-    You can type the guessed word in upper or lower case.
-    If the letter guessed is in the correct position, you will see the letter in the hint.
-    If the letter guessed is not in the word, you will see a '_' in the hint.
-    Try to guess the word with the least amount of guesses.
-    Press Ctrl+C to quit.
-    Good luck!
+Welcome to the Hacking mini game!
+The objective of the game is to find the password in the computer's memory.
+You must guess a 7 letter word.
+You have 4 guesses.
+You can type the guessed word in upper or lower case.
+If the letter guessed is in the correct position, you will see the letter in the hint.
+If the letter guessed is not in the word, you will see a '_' in the hint.
+Try to guess the word with the least amount of guesses.
+Press Ctrl+C to quit.
+Good luck!
     """)
         printComputerMemory()
         word = random.choice(WORDS)
@@ -55,6 +55,14 @@ def main():
                 print()
         if correct:
             print("You guessed the correct word!")
+            cont = input("Do you want to continue? (y/n): ")
+            if cont.lower() == 'n':
+                break
+            elif cont.lower() == 'y':
+                continue
+            else:
+                print("Invalid input.")
+                break
         else:
             print("You ran out of guesses.")
             print("The correct word was", word)
