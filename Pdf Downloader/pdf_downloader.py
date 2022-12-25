5be06613a5ce638c7aaee7efab1705116ccfaa4e
Govind Kushwaha <83280091+G0V1NDK@users.noreply.github.com>
2022-12-25 13:15:36 +0530

    updated the GUI version

--- Pdf Downloader/pdf_downloader.py
+++ Pdf Downloader/pdf_downloader.py
@@ -1,12 +1,37 @@
-# python program to download pdf from url
+# python program to download pdf from url with GUI
 
 import requests
+import tkinter as tk
 
-# enter the url containing the .pdf extension
-url='https://www.thh.nhs.uk/documents/_Departments/Research/InfoSheets/16_sampling_research.pdf'
-r = requests.get(url, stream=True)
+# GUI
 
-# paste the path where you want your file to be downloaded (eg:C/download/myfile.pdf)
-# "<filename>.pdf" must be included in the path for creating and saving the file.
-with open('<pathname>/myfile.pdf', 'wb') as f:
-    f.write(r.content)
+def input_data():
+    name = textfield.get()
+    url = name
+    r = requests.get(url, stream=True)
+    # download_data
+    # You need to add the downloading path here
+    with open('/Users/govindkushwaha/Programming/Python/Pdf Downloader/myfile-new.pdf', 'wb') as f:
+            f.write(r.content)
+
+
+root = tk.Tk()
+root.geometry("1000x800")
+root.title("PDF Downloader")
+f = ("poppins", 25, "bold")
+
+# For importing the image
+# NOTE:-If image is not visible then add the (logo.png) image path from the image folder
+banner = tk.PhotoImage(file="/Users/govindkushwaha/Programming/Python/Pdf Downloader/image/logo.png")
+bannerlabel = tk.Label(root, image=banner)
+bannerlabel.pack()
+
+# creating text box
+textfield = tk.Entry(root, width = 50)
+textfield.pack()
+
+# get data button
+gbtn = tk.Button(root, text="Download", font=f, relief='solid', command=input_data)
+gbtn.pack()
+
+root.mainloop()
