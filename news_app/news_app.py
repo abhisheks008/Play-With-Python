09ab41a6c60713217b5516171f72cde02e3cc94d
B C SAMRUDH <samrudhbc@gmail.com>
2023-01-20 18:41:20 +0530

    I have added a new python project

---
+++ news_app/news_app.py
@@ -0,0 +1,41 @@
+#importing all the required libraries
+
+import requests
+from bs4 import BeautifulSoup
+import tkinter as tk
+from datetime import date
+
+#creating a new window
+window=tk.Tk()
+window.title("News App")
+window.geometry('1000x1000')
+
+#words used to clean the extracted data
+words=['Get in touch','Mobile app','News daily newsletter','BBC World News TV','BBC World Service Radio']
+
+#function used to extract the news
+def get_news():
+        try:
+                news=""
+                response=requests.get('https://www.bbc.com/news')
+                soup = BeautifulSoup(response.text,'html.parser')
+                s = soup.find('body')
+                content=s.find_all('h3')
+                for x in content:
+                        if x.text.strip() not in words:
+                                news+="\n"+x.text.strip()
+                news_label=tk.Label(window,text=news)
+                news_label.pack()
+
+        
+        #Exception Handling
+        except requests.exceptions.RequestException as e:
+                error_label=tk.Label(window,text="Error: Unable to retrieve news:"+e)
+                error_label.pack()
+
+
+date_label=tk.Label(window,text=date.today())
+date_label.pack()
+get_news_button=tk.Button(window,text="Get News",command=get_news)
+get_news_button.pack()
+window.mainloop()
---
+++ news_app/readme.md
@@ -0,0 +1,46 @@
+**A NEWS APP**
+
+**GOAL**
+
+To Create a News App 
+
+**DESCRIPTION**
+
+It is a simple python script to extract news from the web through web scrapping
+
+PLEASE MAKE SURE THAT YOU ARE CONNECT TO THE INTERNET or
+else you wont be able to get the news.
+
+**WHAT I HAD DONE**
+
+Download beautifulsoup4 from  terminal using the command "pip install beautifulsoup4"
+
+STEPS:
+
+1.Imported the required libraries
+
+2.Created a Tkinter window
+
+3.Defined a function get_news() that get the news from the web using requests and beautifulsoup
+
+4.Cleaned the unwanted data and then presented the news
+
+5.Created the date label and a "Get News" Button
+
+6.Exceptions have been handled
+
+**LIBRARIES NEEDED**
+
+ beautifulsoup4, requests, tkinter, datetime
+
+**DEMONSTRATION**
+
+1.The tkinter window opens with todays date and a button with "Get News" on it
+![Image1](images/image1.png)
+
+2.When you click the "Get News" button,the news is displayed
+![Image2](images/image2.png)
+
+**YOUR NAME**
+
+ B C SAMRUDH
