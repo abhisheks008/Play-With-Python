f2a05eede91962e40d6f38db54b79bda36e04e48
pooranjoyb <pooranjoyb2016@gmail.com>
2023-02-11 23:43:05 +0530

    Designed initial UI

---
+++ Covid Tracker App/README.md
@@ -0,0 +1,11 @@
+# Covid Tracker Application
+
+A Covid Tracking Application for India that will fetch information about the current situation of Covid affected people, deaths, etc from an API and
+serve them in the frontend.
+
+
+## Tools & Languages
+
+- Python
+- KivyMD
+- RapidAPI
---
+++ Covid Tracker App/main.py
@@ -0,0 +1,27 @@
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.relativelayout import MDRelativeLayout
+from kivy.core.text import LabelBase
+
+#Defining Window Size
+Window.size = (600, 600)
+
+class WindowManager(ScreenManager):
+    pass
+
+class Covid_Tracker(MDApp):
+    def build(self):
+        self.screen = Builder.load_file("./components/main.kv")
+        return self.screen
+
+Covid_Tracker().run()
+
+
+                
+        
+
+        
